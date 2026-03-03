1bacb185e8ff5a065f9f60b417053799ac90a0fb
TorioCrema <zamasimone5@gmail.com>
2026-03-03 13:22:00 +0100

    feat: implement knight behaviour tests

--- src/test/java/it/unibo/pps/e2/LogicTest.java
+++ src/test/java/it/unibo/pps/e2/LogicTest.java
@@ -1,8 +1,7 @@
 package it.unibo.pps.e2;
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LogicTest {
 
@@ -13,7 +12,7 @@ public class LogicTest {
     @BeforeEach
     public void init() {
         this.pawnPosition = new Pair<>(3, 2);
-        this.knightPosition = new Pair<>(4, 5);
+        this.knightPosition = new Pair<>(4, 4);
         this.logic = new LogicsImpl(10, this.pawnPosition, this.knightPosition);
     }
 
@@ -29,4 +28,24 @@ public class LogicTest {
         assertThrows(IndexOutOfBoundsException.class, () -> this.logic.hit(5, -5));
     }
 
+    @Test
+    public void testKnightTakes() {
+        assertTrue(this.logic.hit(this.pawnPosition.getX(), this.pawnPosition.getY()));
+        assertTrue(this.logic.hasPawn(this.pawnPosition.getX(), this.pawnPosition.getY()));
+        assertTrue(this.logic.hasKnight(this.pawnPosition.getX(), this.pawnPosition.getY()));
+    }
+
+    @Test
+    public void testKnightMovesWithoutTaking() {
+        final Pair<Integer, Integer> validMove = new Pair<>(2, 5);
+        assertFalse(this.logic.hit(validMove.getX(), validMove.getY()));
+        assertTrue(this.logic.hasKnight(validMove.getX(), validMove.getY()));
+    }
+
+    @Test
+    public void testKnightStaysWhenMoveIsInvalid() {
+        final Pair<Integer, Integer> invalidMove = new Pair<>(3, 5);
+        assertFalse(this.logic.hit(invalidMove.getX(), invalidMove.getY()));
+        assertTrue(this.logic.hasKnight(this.knightPosition.getX(), this.knightPosition.getY()));
+    }
 }
