d9f0bd6fc3ac744ac7e8f74e2987e5066b21d183
TorioCrema <zamasimone5@gmail.com>
2026-03-03 14:28:28 +0100

    refactor: KnightTest for readability

--- src/test/java/it/unibo/pps/e2/KnightTest.java
+++ src/test/java/it/unibo/pps/e2/KnightTest.java
@@ -9,11 +9,15 @@ public class KnightTest {
 
     private Knight knight;
     private Pair<Integer, Integer> initialPosition;
+    private Pair<Integer, Integer> validMove;
+    private Pair<Integer, Integer> invalidMove;
 
     @BeforeEach
     public void init() {
         this.initialPosition = new Pair<>(2, 3);
         this.knight = new KnightImpl(this.initialPosition);
+        this.validMove = new Pair<>(4, 4);
+        this.invalidMove = new Pair<>(2, 2);
     }
 
     @Test
@@ -22,27 +26,23 @@ public class KnightTest {
     }
 
     @Test
-    public void testIsValidOnValidMove() {
-        final Pair<Integer, Integer> validMove = new Pair<>(4, 4);
+    public void testIsValidIsTrueOnValidMove() {
         assertTrue(this.knight.isValid(validMove));
     }
 
     @Test
-    public void testIsValidOnInvalidMove() {
-        final Pair<Integer, Integer> invalidMove = new Pair<>(2, 2);
+    public void testIsValidIsFalseOnInvalidMove() {
         assertFalse(this.knight.isValid(invalidMove));
     }
 
     @Test
     public void testMovesOnValidMove() {
-        final Pair<Integer, Integer> validMove = new Pair<>(4, 4);
-        this.knight.move(validMove);
+        this.knight.move(this.validMove);
         assertEquals(validMove, this.knight.getPosition());
     }
 
     @Test
     public void testMoveThrowsOnInvalidMove() {
-        final Pair<Integer, Integer> invalidMove = new Pair<>(2, 2);
         assertThrows(IllegalArgumentException.class, () -> this.knight.move(invalidMove));
     }
 }
